a4c48248366c2b01c31b399db53350b93fa9b78e
tbk323 <2030161848@qq.com>
2022-09-26 11:10:33 +0800

    更新

--- education-app/pages/user-list/user-list.vue
+++ education-app/pages/user-list/user-list.vue
@@ -4,44 +4,76 @@
 				<!-- 登录成功 -->
 				<div class="login" >
 					<span>头像</span>
-					<img src="../../static/M.png" alt="">
+					<img :src="loginlist.imageUrl" alt="">
 					<span class="triangle"></span>
 				</div>
 				
 				<!-- 下面部分 -->
 				<div class="login-one">
 					<p>用户名</p>
+					<span>{{loginlist.nickName}}</span>
 					<span class="triangle"></span>
 				</div>
 				<div class="login-one">
 					<p>手机号</p>
+					<span>{{loginlist.mobile}}</span>
 					<span class="triangle"></span>
 				</div>
 				
 				<view class="cen"></view>
 				<div class="login-one">
 					<p>昵称</p>
+					<span>{{loginlist.username}}</span>
 					<span class="triangle"></span>
 				</div>
 				<div class="login-one">
 					<p>性别</p>
+					<span v-if="loginlist.sex ==1">男</span>
+					<span v-else>女</span>
 					<span class="triangle"></span>
+				</div>
+				<!-- 退出登录· -->
+				<div class="bto">
+					<!-- <button @click="backlogin">退出登录</button> -->
+					<button  @click="dialogToggle('success')">
+						<text class="button-text success-text">成功</text></button>
 				</div>
 			</div>
 	</view>
 </template>
 
 <script>
+	import {reactive,toRefs} from 'vue'
 	export default {
-		data() {
+		setup() {
+			const data=()=>{
+				loginlist:[]
+			}
+			data.loginlist=JSON.parse(localStorage.getItem('userInfo')) ? JSON.parse(localStorage.getItem('userInfo')):[]
+			// 退出登录
+			const backlogin=()=>{
+				// localStorage.clear()
+				uni.navigateTo({
+					url:'../login/login'
+				})
+			}
 			return {
-				
+				backlogin,
+				...toRefs(data)
 			};
 		}
 	}
 </script>
 
 <style lang="scss">
+	.bto{
+		width: 100%;
+		height: 100%;
+		margin-top: 20px;
+		button{
+			background-color: #ffffff;
+		}
+	}
 .triangle{
 		display: inline-block;
 		width: 10px;
@@ -64,15 +96,21 @@
 		display: flex;
 		// justify-content: space-between;
 		align-items: center;
-		span{
+		.triangle{
 			right: 20px;
 			position: absolute;
 			font-weight: 700;
 			font-size: 20px;
 			color: gray;
 		}
+		span{
+			font-size: 14px;
+			right: 40px;
+			color: gray;
+			position: absolute;
+		}
 	}
-	
+	// 顶部资料
 	.login{
 		position: relative;
 		padding: 10px;
@@ -92,10 +130,11 @@
 			font-size: 23px;
 		}
 		img{
-			margin-right: 10px;
+			margin-left: 250px;
 			width: 70px;
 			height: 70px;
 			border-radius: 50px;
+			
 		}
 		p{
 			font-size: 14px;
